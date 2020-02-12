50b5c920004b9b7878c566d5a46e1173e95770fb
LiliwoL <github@liliwol.fr>
2020-02-12 14:46:25 +0100

    Ajout de champs au formulaire de partage

--- src/Form/ShareMovieMailType.php
+++ src/Form/ShareMovieMailType.php
@@ -6,13 +6,24 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 class ShareMovieMailType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dest')
-            ->add('message')
+            // use Symfony\Component\Form\Extension\Core\Type\EmailType;
+            ->add('dest', EmailType::class)
+            // use Symfony\Component\Form\Extension\Core\Type\TextType;
+            ->add('object', TextType::class)
+            // use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+            ->add('message', TextareaType::class)
+            // use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+            ->add('send', SubmitType::class)
         ;
     }
 
